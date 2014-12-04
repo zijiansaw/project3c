2a026ec602bbab4d12e4593929bafb09e257613f
zijiansaw <zijian.saw@drake.edu>
2014-12-04 16:16:26 -0600

    changed the background and shop image

--- src/edu/drake/project3c/SecondFragment.java
+++ src/edu/drake/project3c/SecondFragment.java
@@ -1,16 +1,54 @@
 package edu.drake.project3c;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class SecondFragment extends Fragment {
+	
+	ImageView dailyView1,dailyView2,dailyView3,dailyView4,dailyView5,dailyView6,dailyView7;
+	
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
 		View view = inflater.inflate(R.layout.activity_second_tab, container,false);
+		
+		int images[] = {getResources().getIdentifier("drawable/fact1", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact2", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact3", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact4", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact5", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact6", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact7", null, getActivity().getPackageName()),
+				getResources().getIdentifier("drawable/fact8", null, getActivity().getPackageName())};
+		
+		dailyView1 = (ImageView) view.findViewById(R.id.imageView1);
+		dailyView2 = (ImageView) view.findViewById(R.id.imageView2);
+		dailyView3 = (ImageView) view.findViewById(R.id.imageView3);
+		dailyView4 = (ImageView) view.findViewById(R.id.imageView4);
+		dailyView5 = (ImageView) view.findViewById(R.id.imageView5);
+		dailyView6 = (ImageView) view.findViewById(R.id.imageView6);
+		dailyView7 = (ImageView) view.findViewById(R.id.imageView7);
+		
+		Calendar dayOfYear = Calendar.getInstance();
+		int dayOfWeek = dayOfYear.get(Calendar.DAY_OF_WEEK);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		//put your codes here
 		//if you want to initialize image/buttons, insert view. in front of findViewById 
