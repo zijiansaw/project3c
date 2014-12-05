a080940f32f7172dba491c5577f4a00c53c31c95
nsGiuliani <nicholas.giuliani@drake.edu>
2014-12-05 17:17:46 -0600

    tried to impliment settings page and could not get it to link to anouther activity

--- gen/edu/drake/project3c/R.java
+++ gen/edu/drake/project3c/R.java
@@ -1284,11 +1284,15 @@ containing a value of this type.
         public static final int activity_first_tab=0x7f030018;
         public static final int activity_main=0x7f030019;
         public static final int activity_second_tab=0x7f03001a;
-        public static final int activity_third_tab=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_settingspage=0x7f03001b;
+        public static final int activity_third_tab=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int settings=0x7f0c0001;
+        public static final int settings_page=0x7f0c0002;
+        public static final int settingspage=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1334,6 +1338,9 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_settings=0x7f0a0010;
+        public static final int title_activity_settings_page=0x7f0a0011;
+        public static final int title_activity_settingspage=0x7f0a0012;
     }
     public static final class style {
         /** 
--- src/edu/drake/project3c/MainActivity.java
+++ src/edu/drake/project3c/MainActivity.java
@@ -19,6 +19,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -65,6 +67,23 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 			}
 		});
 		
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
